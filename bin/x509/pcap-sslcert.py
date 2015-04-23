0d90f20ed7407dc60c741ba0c17b6168a242d551
Alexandre Dulaunoy <a@foo.be>
2015-04-23 13:59:00 +0200

    Handle unset DN

--- bin/x509/pcap-sslcert.py
+++ bin/x509/pcap-sslcert.py
@@ -73,7 +73,10 @@ for l in fileinput.input(args.r):
         if args.j:
             print (json.dumps(c))
         elif args.f:
-            print (c['dstip']+","+c['fp']+","+x509.get_subject().CN)
+            subject = x509.get_subject().CN
+            if subject is None:
+                subject = ""
+            print (c['dstip']+","+c['fp']+","+subject
         elif args.s:
             print (c['fp']+","+base64.standard_b64encode(dercert))
         else:
